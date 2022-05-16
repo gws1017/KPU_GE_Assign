db6fd73e2f8db272474321b0b5850d1c8b6cba8c
gws1017 <gws1017@naver.com>
2022-05-16 22:37:15 +0900

    [셰이더 프로그래밍] 프래그먼트 셰이더#3
    - 셰이더 코드 함수 분리
    - 철조망 패턴 그리기
    - 여러개의 동심원 그리기 -> 시험 출제 가능

--- 셰이더 프로그래밍/GLSLBase/Shaders/FSSandbox.fs
+++ 셰이더 프로그래밍/GLSLBase/Shaders/FSSandbox.fs
@@ -3,9 +3,22 @@
 layout(location=0) out vec4 FragColor;
 in vec4 v_Color;
 
-void main()
+
+const float PI = 3.141592;
+
+vec4 CrossPattern()
 {
-	/*float dis = distance(v_Color.xy,vec2(0.5,0.5));
+	vec4 returnValue = vec4(1,1,1,1);
+	float XAxis = sin(10*(v_Color.x * 2 * PI + PI / 4.0f)); // 끝 깔끔 -> 시험 (PI / 4.0f 정답)
+	float YAxis = sin(10*(v_Color.y * 2 * PI + PI / 4.0f));
+	float resultColor = max(XAxis,YAxis);
+	returnValue = vec4(resultColor);
+	return returnValue;
+}
+
+vec4 DrawCircle()
+{
+	float dis = distance(v_Color.xy,vec2(0.5,0.5));
 	
 	vec4 newColor = vec4(0, 0, 0, 0);
 
@@ -16,16 +29,35 @@ void main()
 	else
 	{
 		newColor = vec4(0, 0, 0, 0);
-	}*/
-	/*if(v_Color.y > 0.5)
-		FragColor = vec4(1);
-	else
-		FragColor = vec4(0);*/
-
+	}
+	return newColor;
+}
+vec4 DrawCircleLine()
+{
 	float dis = distance(v_Color.xy,vec2(0.5,0.5));
+
+	vec4 newColor = vec4(0, 0, 0, 0);
+
 	if(dis > 0.48 && dis < 0.5)
-		FragColor = vec4(1);
+		newColor = vec4(1);
 	else
-		FragColor = vec4(0);
-	//FragColor = newColor;
+		newColor = vec4(0);
+
+	return newColor;
+}
+
+vec4 DrawMultipleCircles()
+{
+	float dis = distance(v_Color.xy,vec2(0.5,0.5));// 0 ~ 0.5f
+	float temp = sin(10 *dis * 4 * PI); //시험 나올듯
+	return vec4(temp);
+	
+}
+
+void main()
+{
+	//FragColor = CrossPattern();
+	//FragColor = DrawCircle();
+	//FragColor = DrawCircleLine();
+	FragColor = DrawMultipleCircles();
 }
