3359f4df0dd98aea3ca3a8977507ad857854c07e
gws1017 <65538479+gws1017@users.noreply.github.com>
2022-06-11 13:52:26 +0900

    [셰이더 프로그래밍] 텍스처 매핑 #3
    - 텍스처좌표 꼬아서 그리기 문제1, 문제2
    
    - 텍스처좌표 꼬아서 그리기 문제3,4 (숙제 ,시험 출시 가능)
    - 문제3은 문제 2에서 y만 수정한것
    - 문제4는 x는 그대로 두고 y만 수정, abs floor fract 사용

--- 셰이더 프로그래밍/GLSLBase/Shaders/TextureSandbox.fs
+++ 셰이더 프로그래밍/GLSLBase/Shaders/TextureSandbox.fs
@@ -15,8 +15,55 @@ vec4 Flip()
 	return returnValue;
 }
 
+vec4 P1()
+{
+	vec4 returnValue = vec4(0);
+	vec2 newTexCoord = v_TexCoord;
+	newTexCoord.y = abs(2.0f*(v_TexCoord.y -0.5f));
+	returnValue = texture(u_TexSampler,newTexCoord);
+
+	return returnValue;
+}
+
+vec4 P2()
+{
+	vec4 returnValue = vec4(0);
+	vec2 newTexCoord = v_TexCoord;
+	newTexCoord.x = fract(3.0f*v_TexCoord.x);
+	newTexCoord.y = newTexCoord.y/3.0 + floor(3.0f*v_TexCoord.x) / 3.0;
+	returnValue = texture(u_TexSampler,newTexCoord);
+
+	return returnValue;
+}
+
+vec4 P3() //P2 에서 y만 변경하면된다
+{
+	vec4 returnValue = vec4(0);
+	vec2 newTexCoord = v_TexCoord;
+	float newValue = abs((v_TexCoord.x-1.0) * 3.0f);
+	newTexCoord.x = fract(3.0f*v_TexCoord.x);
+	newTexCoord.y = newTexCoord.y/3.0 + floor(newValue) / 3.0;
+	returnValue = texture(u_TexSampler,newTexCoord);
+
+	return returnValue;
+}
+
+vec4 P4() //P2 에서 y만 변경하면된다
+{
+	vec4 returnValue = vec4(0);
+	vec2 newTexCoord = v_TexCoord;
+	newTexCoord.y = fract(3.0f*v_TexCoord.y)/3.0 +floor(abs((v_TexCoord.y - 1.0)*3.0))/3.0;
+	returnValue = texture(u_TexSampler,newTexCoord);
+
+	return returnValue;
+}
+
 void main()
 {
-	FragColor = Flip();
+	//FragColor = Flip();
+	//FragColor = P1();
+	//FragColor = P2();
+	//FragColor = P3();
+	FragColor = P4();
 	//FragColor = vec4(1,1,1,1);
 }
